787728ffd782a75c63782b3d9286a57ed4fcd120
wuyin517 <1937896181@qq.com>
2023-11-09 17:31:12 +0800

    Add files via upload

--- Locale.thy
+++ Locale.thy
@@ -11,6 +11,7 @@ fixes delete :: "'a \<Rightarrow> 't \<Rightarrow> 't"
 fixes lookup :: "'t \<Rightarrow> 'a \<Rightarrow> bool"
 fixes  invar :: "'t \<Rightarrow> bool"
 fixes  to_set:: "'t \<Rightarrow> 'a set"
+fixes  is_sub:: "'a \<Rightarrow> 'a \<Rightarrow> bool"
 fixes  match :: "'t \<Rightarrow> 'a \<Rightarrow> 'a list"
 
 assumes  invar_empty:  "invar empty"
@@ -18,12 +19,13 @@ assumes invar_insert: "invar t \<Longrightarrow> invar(insert x t)"
 assumes invar_delete: "invar t \<Longrightarrow> invar(delete x t)"
 assumes   insert_set:   "to_set (insert x t) = to_set t \<union> {x}"
 assumes   delete_set:   "invar t \<Longrightarrow> to_set (delete x t) = to_set t - {x}"
-assumes match_correctness: " \<forall>xl\<in>set (match t l). lookup t xl"
+assumes match_correctness: " \<forall>xl\<in>set (match t l). (lookup t xl) \<and> is_sub xl l"
 
 
 interpretation FMPM: Match
-where empty = empty_trie and lookup = lookup_trie and insert = insert_trie and delete = delete_trie
-and invar = invar_trie and to_set = trie_set and match = FMPM
+  where empty = empty_trie and lookup = lookup_trie and insert = insert_trie 
+  and delete = delete_trie and invar = invar_trie and to_set = trie_set 
+  and is_sub = is_Substring and match = FMPM
 proof (standard, goal_cases)
   case 1
   then show ?case by (simp add:empty_trie_def)
--- match.thy
+++ match.thy
@@ -2,43 +2,10 @@ theory match
   imports Main Trie_plus
 begin
 
-
 (***chinese/english ***)
 datatype ch = English nat | Chinese nat nat
 
-(*** 中文 为人民服务  ***)
-definition "list1 = [Chinese 206 170,Chinese 200 203,Chinese 195 241,Chinese 183 254,Chinese 206 241]"
-(*** 中文 人民日报  ***)
-definition "list2 = [Chinese 200 203,Chinese 195 241,Chinese 200 213,Chinese 177 168]"
-(*** 中文 服务社会  ***)
-definition "list3 = [Chinese 183 254,Chinese 206 241,Chinese 201 231,Chinese 187 225]"
-(***英文 love  ***)
-definition "list4 = [English 108, English 111, English 118, English 101]"
-(*** 中文 人民  ***)
-definition "list5 = [Chinese 200 203,Chinese 195 241]"
-
-definition "list6 = []"
-(**只包含list1 list2 list3 list5**)
-definition "trie = Trie_plus False
-  [(Chinese 206 170,
-    Trie_plus False
-     [(Chinese 200 203,
-       Trie_plus False
-        [(Chinese 195 241,
-          Trie_plus False
-           [(Chinese 183 254, Trie_plus False [(Chinese 206 241, Trie_plus True [])])])])]),
-   (Chinese 200 203,
-    Trie_plus False
-     [(Chinese 195 241,
-       Trie_plus True [(Chinese 200 213, Trie_plus False [(Chinese 177 168, Trie_plus True [])])])]),
-   (Chinese 183 254,
-    Trie_plus False
-     [(Chinese 206 241,
-       Trie_plus False
-        [(Chinese 201 231, Trie_plus False [(Chinese 187 225, Trie_plus True [])])])])]"
-
-value trie
-
+(**** FMPM  ****)
 fun look_up::"ch trie \<Rightarrow> ch list \<Rightarrow> ch list" where
 "look_up (Trie_plus b m) [] = []"|
 "look_up (Trie_plus b m) (x#xs) = (case map_of m x of
@@ -55,17 +22,58 @@ fun FMPM::"ch trie \<Rightarrow>ch list \<Rightarrow> ch list list" where
                                  FMPM (Trie_plus b m) xs | sx \<Rightarrow> sx # FMPM (Trie_plus b m) xs)"
 
 
-(*** proof ****)
+
+(*** substring ***)
+definition is_substring :: "'a list \<Rightarrow> 'a list \<Rightarrow> bool" where
+  "is_substring s1 s2 = (\<exists>s3 s4. s2 = s3 @ s1 @ s4)"
+
 fun startsWith :: "'a list \<Rightarrow> 'a list \<Rightarrow> bool"
   where "startsWith [] ys = True" |
         "startsWith (x # xs) [] = False" |
         "startsWith (x # xs) (y # ys) = (if x = y then startsWith xs ys else False)"
-value "startsWith  ''ab'' ''abcde''"
-
-(*** 字串 ***)
 
 
-value "look_up trie [Chinese 206 170, Chinese 200 203, Chinese 196 241, Chinese 183 254, Chinese 205 241]"
+fun is_Substring :: "'a list \<Rightarrow> 'a list \<Rightarrow> bool" where
+  "is_Substring x [] = (x = [])" |
+  "is_Substring x (y # ys) = (startsWith x (y # ys) \<or> is_Substring x ys)"
+value "is_Substring  ''bc'' ''abcde''"
+
+
+(***is_Substring proof ****)
+lemma is_sub_is_sub:"startsWith xs ys = (\<exists>ls. xs@ls = ys)" 
+  by(induct xs ys rule: startsWith.induct) auto
+
+lemma startsWith_sub:"startsWith x ys \<Longrightarrow> is_substring x ys"
+  by(auto simp:is_sub_is_sub is_substring_def) 
+
+lemma is_substring_add:"is_substring x ys \<Longrightarrow> is_substring x (y#ys)" 
+  apply(simp add: is_substring_def) 
+  apply(erule exE)+
+  apply (rule_tac x="y # s3" in exI)
+  apply (rule_tac x="s4" in exI)
+  apply simp
+  done
+
+theorem is_Substring_equiv_is_substring:
+  "is_Substring x y = is_substring x y"
+proof(induct x y rule: is_Substring.induct)
+  case (1 x)
+  then show ?case 
+    by (simp add: is_substring_def)
+next
+  case (2 x y ys)
+  then show ?case 
+  proof(cases)
+    assume 01:"startsWith x (y#ys)" thus ?case using 2 is_Substring.simps(2)[of x y ys]
+      by (metis  startsWith_sub)
+  next
+    assume 02:"\<not> startsWith x (y#ys)" thus ?case using 2 is_substring_add is_Substring.simps(2) 
+      by (smt (verit, best) append_self_conv2 is_sub_is_sub is_substring_def list.sel(3) tl_append2)
+  qed
+qed
+
+
+
 
 lemma look_up_empty:"look_up t [] = []" 
   apply (induct t)
@@ -84,7 +92,7 @@ lemma look_up_cases:"l\<noteq> [] \<Longrightarrow> look_up (Trie_plus b m) l = 
   done 
 
 
-lemma ll5:"startsWith xs ys \<Longrightarrow> startsWith (a#xs) (a#ys)"
+lemma startsWith_add:"startsWith xs ys \<Longrightarrow> startsWith (a#xs) (a#ys)"
   by(induct xs ys rule:startsWith.induct) auto
 
 lemma look_up_pre:"startsWith (look_up (Trie_plus b m) l) l"
@@ -104,7 +112,7 @@ next
   qed
 qed
 
-lemma ll7:"lookup_trie t l \<Longrightarrow> look_up t l = l"
+lemma lookup_trie_look_up:"lookup_trie t l \<Longrightarrow> look_up t l = l"
   proof(induct t l rule: lookup_trie.induct)
     case (1 b m)
     then show ?case by simp
@@ -130,7 +138,6 @@ lemma prefix_lookup_nil:"\<not>(lookup_trie t (look_up t ks))  \<Longrightarrow>
 lemma prefix_lookup_conv:"(lookup_trie t (look_up t ks))  \<Longrightarrow> prefix_lookup t ks = (look_up t ks) "
   by auto
 
-value "prefix_lookup trie [Chinese 206 170, Chinese 200 203, Chinese 196 241, Chinese 183 254, Chinese 205 241]"
 
 lemma prefix_lookup_eq:"lookup_trie t l \<Longrightarrow> prefix_lookup t l = l" 
   proof(induct t l rule: lookup_trie.induct)
@@ -145,11 +152,11 @@ lemma prefix_lookup_eq:"lookup_trie t l \<Longrightarrow> prefix_lookup t l = l"
     next
       case (Some st)
       then show ?thesis using 2 
-        by (simp add: ll7)        
+        by (simp add: lookup_trie_look_up)        
   qed
 qed
 
-lemma lll3:"prefix_lookup t l = str \<Longrightarrow> startsWith str l "
+lemma prefix_lookup_startsWith:"prefix_lookup t l = str \<Longrightarrow> startsWith str l "
 proof(induct t )
   case (Trie_plus b kvs)
   then show ?case 
@@ -157,12 +164,12 @@ proof(induct t )
 qed
 
 
-lemma l0:"FMPM t [] = []"
+lemma FMPM_empty':"FMPM t [] = []"
   apply(induct t)
   by auto
-lemma FMPM_emoty:"FMPM empty_trie l = []"
+lemma FMPM_empty:"FMPM empty_trie l = []"
   apply(induct l)
-  using l0 apply blast
+  using FMPM_empty' apply blast
   by (simp add: empty_trie_def)
 
 
@@ -180,13 +187,36 @@ next
     by (metis list.sel(1) list.simps(5) prefix_lookup_eq FMPM.simps(2))
 qed
 
-lemma q2:"prefix_lookup t l \<noteq> [] \<Longrightarrow>lookup_trie t (hd (FMPM t l))"
+lemma match_is_sub:"\<forall>x\<in>set (FMPM t l).  is_Substring  x l"
+proof(induction t l rule: FMPM.induct)
+  case (1 b m)
+  then show ?case 
+    by auto
+next
+  case (2 b m x xs)
+  then show ?case 
+    proof(cases "prefix_lookup (Trie_plus b m) (x#xs)" )
+      case Nil
+      then show ?thesis 
+        by (simp add: "2.IH"(1))
+    next
+      case (Cons a list)
+      then show ?thesis  apply auto 
+        apply (metis Cons prefix_lookup_startsWith startsWith.simps(3))
+        using "2.IH"(2)Cons apply blast
+         apply (metis prefix_lookup_startsWith Cons startsWith.simps(3))
+        using "2.IH"(2) Cons by blast
+    qed
+qed
+
+
+lemma prefix_lookup_hd:"prefix_lookup t l \<noteq> [] \<Longrightarrow>lookup_trie t (hd (FMPM t l))"
   apply(induction t l rule: FMPM.induct)
    apply simp
   by (metis (no_types, lifting) FMPM.simps(2) hd_Cons_tl list.case_eq_if list.sel(1) prefix_lookup_conv prefix_lookup_nil)
 
 
-theorem match_correct:"\<forall>x\<in>set (FMPM t l). lookup_trie t x"
+lemma match_lookup_trie:"\<forall>x\<in>set (FMPM t l). lookup_trie t x"
   proof(induction t l rule: FMPM.induct)
     case (1 b m)
     then show ?case by simp
@@ -199,11 +229,15 @@ theorem match_correct:"\<forall>x\<in>set (FMPM t l). lookup_trie t x"
         using "2.IH"(1) by fastforce
     next
       case (Cons a list)
-      then show ?thesis 
-        by (metis "2.IH"(2) FMPM.simps(2) list.distinct(1) list.simps(5) prefix_lookup_lookup_trie set_ConsD)
+      then show ?thesis
+        by (metis "2.IH"(2) FMPM.simps(2) list.distinct(1) list.simps(5) 
+              prefix_lookup_lookup_trie set_ConsD)
     qed
   qed
 
+theorem match_correct:"\<forall>x\<in>set (FMPM t l). (lookup_trie t x)\<and>is_Substring x l"
+  using match_lookup_trie match_is_sub by blast
 
+export_code empty_trie insert_trie lookup_trie delete_trie FMPM in Haskell
 
 end
